e2e09e4d55f155db30a66ecb6716ffce98f8ac2e
Andre Nepomuceno <andre.nepa@gmail.com>
2022-05-06 21:31:55 -0300

    Initial Commit

---
+++ DesafioPickerView1UITests/DesafioPickerView1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DesafioPickerView1UITestsLaunchTests.swift
+//  DesafioPickerView1UITests
+//
+//  Created by André N. dos Santos on 06/05/22.
+//
+
+import XCTest
+
+class DesafioPickerView1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
