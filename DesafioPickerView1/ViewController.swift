b745383c13049d2e2295aeb722c2474d53024fd2
Andre Nepomuceno <andre.nepa@gmail.com>
2022-05-07 10:41:55 -0300

    Commit inicial, ja com o desafio feito em aula

--- DesafioPickerView1/ViewController.swift
+++ DesafioPickerView1/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nomesPickerView: UIPickerView!
+    @IBOutlet weak var nomeLabel: UILabel!
+    
+    let nomes = ["Andre",
+                 "Gabriel",
+                 "Marcelo",
+                 "Francinildo",
+                 "Jessica",
+                 "Adriano"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nomesPickerView.dataSource = self
+        nomesPickerView.delegate = self
+        
     }
 
-
+    @IBAction func botaoTrocarNomeAction(_ sender: UIButton) {
+        let componente = 0 //Nosso picker só tem um component (coluna)
+        let linha: Int = nomesPickerView.selectedRow(inComponent: componente)
+        nomeLabel.text = nomes[linha]
+    }
+    
 }
-
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return nomes.count
+    }
+       
+}
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return nomes[row]
+    }
+}
